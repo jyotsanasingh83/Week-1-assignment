4e67891803619a23a714930585b5e4615c3eebea
jyotsana.singh@gmail.com <jyotsana.singh83@gmail.com>
2023-06-16 16:55:13 +0530

    Completed Assigment

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -7,8 +7,38 @@
   - `npm run test-anagram`
 */
 
+/*
+function sumArray(numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
+}
+
+function calculateSum() {
+  const numbers = [1, 2, 3, 4, 5];
+  const result = sumArray(numbers);
+  document.getElementById('output').innerHTML = 'The sum is: ' + result;
+}
+
+
+const str1 = 'test';
+const str2 = 'abcd';
+*/
 function isAnagram(str1, str2) {
 
+  const sortedStr1 = str1.toLowerCase().split('').sort().join('');
+  const sortedStr2 = str2.toLowerCase().split('').sort().join('');
+  
+  if (sortedStr1==sortedStr2) {
+    console.log('The strings are anagrams.');
+  } else {
+    console.log('The strings are not anagrams.');
+  }
+
 }
+
+isAnagram(str1, str2);
 
 module.exports = isAnagram;
